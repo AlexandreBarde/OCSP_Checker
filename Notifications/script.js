cd681bb2d1de842b1175119dc50eee457f9afb13
Alexandre <barde.alexandre@yahoo.fr>
2017-10-29 22:31:47 +0100

    Affichage notif

--- Notifications/script.js
+++ Notifications/script.js
@@ -1,3 +1,31 @@
+var test = document.getElementById('notif-popup');
+
+var action_reduce = document.getElementById('action_reduce');
+var action_close = document.getElementById('action_close');
+
+action_reduce.style.cursor = 'pointer';
+action_close.style.cursor = 'pointer';
+test.style.cursor = 'pointer';
+
+action_reduce.onclick = function(){
+  document.getElementById('notif').style.visibility = 'hidden';
+  document.getElementById('notif-popup').style.visibility = 'visible';
+}
+
+action_close.onclick = function(){
+  document.getElementById('notif').style.visibility = 'hidden';
+  document.getElementById('notif-popup').style.visibility = 'hidden';
+}
+
+test.onclick = function(){
+  document.getElementById('notif').style.top = 'auto';
+  document.getElementById('notif').style.bottom = '5%';
+  document.getElementById('notif').style.left = '3%';
+  document.getElementById('notif').style.riht = 'auto';
+  document.getElementById('notif-popup').style.visibility = 'hidden';
+  document.getElementById('notif').style.visibility = 'visible';
+}
+
 function afficherFenetre(position)
  {
    switch (position) {
