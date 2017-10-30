be404d5f75a00c12647a99f65dc2af7538a0f3ec
Mathieu <mathieu@localhost.localdomain>
2017-10-30 18:10:11 +0100

    Correction script shell

--- NativeMessaging/url/url_share.py
+++ NativeMessaging/url/url_share.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python -u
 
-import sys, json, struct, datetime
+# coding: utf-8
 
-# Read a message from stdin and decode it.
-def getMessage():
+import sys, json, struct, datetime, subprocess
+
+def get_message():
+  """
+  Lit un message au format JSON reçu dans une struct
+  """
   rawLength = sys.stdin.read(4)
   if len(rawLength) == 0:
       sys.exit(0)
@@ -11,21 +15,33 @@ def getMessage():
   message = sys.stdin.read(messageLength)
   return json.loads(message)
 
-# Encode a message for transmission, given its content.
-def encodeMessage(messageContent):
+def encode_message(messageContent):
+  """
+  Prépare un réponse au format JSON
+  """
   encodedContent = json.dumps(messageContent)
   encodedLength = struct.pack('@I', len(encodedContent))
   return {'length': encodedLength, 'content': encodedContent}
 
-# Send an encoded message to stdout.
-def sendMessage(encodedMessage):
+def send_message(encodedMessage):
+  """
+  Envoie un message encodé sur la sortie standard
+  """
   sys.stdout.write(encodedMessage['length'])
   sys.stdout.write(encodedMessage['content'])
   sys.stdout.flush()
 
+def date_diff(in_date):
+  """
+  Retourne la différence entre la date courante et 
+  celle passée en paramètre
+  """
+  # Appel script shell
+  subprocess.call("../../Script_Dates/date_check")
+
 while True:
-  receivedMessage = getMessage()
+  receivedMessage = get_message()
   if (receivedMessage == "yahoo.fr"):
     date = datetime.datetime.now()
     # Renvoie l'heure courante
-    sendMessage(encodeMessage(str(date)))
+    send_message(encode_message(str(date)))
