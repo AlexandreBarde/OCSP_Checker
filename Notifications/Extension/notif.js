28fba9dae523d0ae61db6965b5b640d5520286a1
Alexandre <barde.alexandre@yahoo.fr>
2017-11-01 17:56:19 +0100

    Rajout d'une fonction pour créer le popup

--- Notifications/Extension/notif.js
+++ Notifications/Extension/notif.js
@@ -1,21 +1,17 @@
-if(location.protocol == 'https:')
-{
+generatePopup("IUT Paul Sabatier Toulouse III", "24 octobre 2017 à 14h", "12h:35m:23s");
 
+
+function generatePopup(website, update, date)
+{
   var div_notif = document.createElement('div');
   var div_titre = document.createElement('div');
   var div_texte = document.createElement('div');
-  var anciennete = document.createElement('p');
-  var maj = document.createElement('p');
 
   div_notif.id = "OCSP_check_div_notif";
   div_titre.id = "OCSP_check_div_titre";
   div_texte.id = "OCSP_check_div_texte";
-  anciennete.id = "OCSP_check_anciennete";
-  maj.id = "OCSP_check_maj";
 
-  div_titre.textContent = "IUT Paul Sabatier Toulouse III"
-  anciennete.textContent = "Ancienneté de l'attestation : 12h:35m:23s";
-  maj.textContent = " Date de la dernière mise à jour du cache OCSP : 24 octobre 2017 à 14h";
+  div_titre.textContent = website;
 
   div_notif.style.width = "400px";
   div_notif.style.height = "100px";
@@ -25,7 +21,7 @@ if(location.protocol == 'https:')
   div_notif.style.left = "3%";
   div_notif.style.fontFamily = "Arial";
   div_notif.style.border = "0";
-    div_notif.style.margin = "0";
+  div_notif.style.margin = "0";
 
   div_titre.style.borderRadius = "4px 4px 0px 0px";
   div_titre.style.backgroundColor = "rgba(207, 0, 15, 1)";
@@ -38,11 +34,46 @@ if(location.protocol == 'https:')
   div_texte.style.fontSize = "18px";
   div_texte.style.fontFamily = "Arial";
 
-  div_texte.appendChild(anciennete);
-  div_texte.appendChild(maj);
+  if(isSecure())
+  {
+    var anciennete = document.createElement('p');
+    var maj = document.createElement('p');
+    anciennete.id = "OCSP_check_anciennete";
+    maj.id = "OCSP_check_maj";
+    anciennete.textContent = "Ancienneté de l'attestation : " + date; //12h:35m:23s
+    maj.textContent = " Date de la dernière mise à jour du cache OCSP : " + update; //24 octobre 2017 à 14h
+    div_texte.appendChild(anciennete);
+    div_texte.appendChild(maj);
+  }
+  else
+  {
+    var texte = document.createElement('p');
+    texte.textContent = "Ce site ne possède pas de certificat";
+    div_texte.appendChild(texte);
+  }
+
   div_notif.appendChild(div_titre);
   div_notif.appendChild(div_texte);
 
   document.body.appendChild(div_notif);
 
 }
+
+function isSecure()
+{
+  if(location.protocol == 'https:')
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+
+
+
+
+
+
