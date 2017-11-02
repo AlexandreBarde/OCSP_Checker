acf3162da590b5d97c85cdd9b3b60dbb657cc565
Mathieu Mandret <mathieu.mandret119@gmail.com>
2017-11-02 15:18:26 +0100

    Récupération du certificat SSL d'un serveur avec un script python.

--- Script_Dates/date_check.py
+++ Script_Dates/date_check.py
@@ -1,9 +1,24 @@
-#!/usr/bin/python3.5
-
 # coding: utf-8
 
-import os
-import sys
+import ssl
+import socket
 
-url = sys.stdin.read()
-os.system("./date_check " + url)
+# Serveur a atteindre
+host = 'www.google.com'
+# HTTPS -> port 443
+port = 443
+cont = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+# Ouverture d'un socket client
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Ouverture d'un socket SSL à partir du socket existant
+ssl_sock = cont.wrap_socket(s, server_hostname=host)
+# Connexion au serveur via le socket SSL
+ssl_sock.connect((host, port))
+# Fermeture de la connexion
+ssl_sock.close()
+# La connexion fonctionne, l'afficher
+print("OK")
+# Récupération du certificat
+cert = ssl.get_server_certificate((host, port))
+# Affichage du certificat x509
+print(cert)
