23519882b2edc45bbfbc551208205c7a406cd4a1
Mathieu Mandret <mathieu.mandret119@gmail.com>
2017-11-27 19:11:27 +0100

    Ca marchait pas mais ca remarche, incompréhensible

--- Script_Dates/url_sender.js
+++ Script_Dates/url_sender.js
@@ -1,12 +1,5 @@
 var port = browser.runtime.connectNative("date_getter");
 
-port.onDisconnect.addListener((p) => {
-    if(p.error) {
-	console.log(`${p.error.message}`);
-    }
-});
-
-
 port.onMessage.addListener((response) => {
     console.log('Recu');
     console.log(response);
@@ -14,5 +7,5 @@ port.onMessage.addListener((response) => {
 
 browser.browserAction.onClicked.addListener(() => {
     console.log("Envoi de l'url");
-    port.postMessage("google.com");
+    port.postMessage("reddit.com");
 });
