86d4df2c910ee4d04c659cf91bad0a91185ea83d
Mathieu Mandret <mathieu.mandret119@gmail.com>
2017-11-29 19:13:51 +0100

    Séparation background et content

---
+++ Prototype/addon/background/background.js
@@ -0,0 +1,5 @@
+/**
+*
+* Ce script fait la liaison entre l'extension et l'application, il doit recevoir l'url depuis le content script, le transmettre
+* à l'application native, recevoir la date et la renvoyer au contenu pour l'afficher dans une notif
+*/
---
+++ Prototype/addon/content/content.js
@@ -0,0 +1,4 @@
+/**
+* Le ou les scripts de content doivent injecter les notifs dans les pages, afficher la liste des site et tout le bordel en front end,  recuperer l'url
+* et l'envoyer au background, et pouvoir recevoir la date et afficher la notif
+*/
