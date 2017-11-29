2fd2b2beb3cd089c07fffecfffa08dac9ec7cd20
Jules Lassara <jules.lssr@gmail.com>
2017-11-29 21:43:20 +0100

    récup url dans background + affiche liste sites dans content

--- Prototype/addon/background/background.js
+++ Prototype/addon/background/background.js
@@ -3,3 +3,11 @@
 * Ce script fait la liaison entre l'extension et l'application, il doit recevoir l'url depuis le content script, le transmettre
 * à l'application native, recevoir la date et la renvoyer au contenu pour l'afficher dans une notif
 */
+
+//Récupération de l'url de l'onglet actif
+browser.tabs.onUpdated.addListener(function(tabId, infos, tab) {
+  //Vérification que l'url est définie (utiliser l'url uniquement dans cette condition)
+  if (typeof infos.url !== 'undefined') {
+    console.info(infos.url);
+  }
+});
--- Prototype/addon/content/content.js
+++ Prototype/addon/content/content.js
@@ -2,3 +2,34 @@
 * Le ou les scripts de content doivent injecter les notifs dans les pages, afficher la liste des site et tout le bordel en front end,  recuperer l'url
 * et l'envoyer au background, et pouvoir recevoir la date et afficher la notif
 */
+
+// Met à jour "l'info-bulle" lorsqu'on ouvre l'extension, un moyen plus simple existe mais pas maintenant, ça fonctionne pour le prototype
+browser.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+
+    // Update info alert
+    updateSiteState(tabs[0].url, true);
+
+});
+
+// Events related to click
+document.addEventListener("click", function(e) {
+    // Follow Site button
+    if (e.target.classList.contains("notfollowed")) {
+        browser.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+            addSite(tabs[0].url, 20);
+            updateSiteState(tabs[0].url, false);
+        });
+    // Unfollow Site button
+    } else if (e.target.classList.contains("followed")) {
+        browser.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+            removeSite(tabs[0].url, 20);
+            updateSiteState(tabs[0].url, false);
+        });
+    // Unfollow All Sites button
+    } else if (e.target.classList.contains("unstorage")) {
+        browser.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+            removeAllSites();
+            updateSiteState(tabs[0].url, false);
+        });
+    }
+});
