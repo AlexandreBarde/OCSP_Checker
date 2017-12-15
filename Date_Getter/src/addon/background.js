577c4efc88f01ce525cd7a077b230a907347e43e
Mathieu Mandret <mathieu.mandret119@gmail.com>
2017-12-15 23:16:36 +0100

    Pas renvoyer le message si on est sur le meme serveur Y'a peut être du code dupliqué mais je commence à voir flou dsl a refractorer du coup

--- Date_Getter/src/addon/background.js
+++ Date_Getter/src/addon/background.js
@@ -1,3 +1,7 @@
+// Variable qui contiendra l'adresse du dernier
+// serveur consulté
+var prec_serv;
+
 // Recupere l'adresse d'un serveur depuis une url
 function getServerAdress(url) {
     // Cree un lien
@@ -8,24 +12,43 @@ function getServerAdress(url) {
     return parser.hostname;
 }
 
+// Envoie un message à l'application native
+// et gere la réponse avec la fonction en
+// parametre
+function sendMessage(msg) {
+    chrome.runtime.sendNativeMessage('com.ptut.date_getter',
+        {url: msg},
+        function (resp) {
+            // Afficher le réponse
+            console.log(resp.text);
+        })
+}
+
 // Executer à chaque changement dans l'onglet courant
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
     // Attendre que la page ai fini de charger
-    if (changeInfo.status == 'complete') {
+    if (changeInfo.status === 'complete') {
         // Recuperer l'url de l'onglet courant
         chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+            // Si ce n'est pas le premier site qu'on consulte
             var url = tabs[0].url;
             // Recuperer le nom d'hôte du serveur
             var hostname = getServerAdress(url);
-            console.log(hostname);
-            // L'envoyer à l'application
-            chrome.runtime.sendNativeMessage('com.ptut.date_getter',
-                {url: hostname},
-                function (resp) {
-                    // Afficher le réponse
-                    console.log(resp);
-                    console.log(resp.text);
-                })
+            // Si c'est pas le premier site qu'on consulte
+            if (typeof prec_serv !== 'undefined') {
+                // Verifier qu'on ne soit pas toujour sur le meme serveur
+                if (hostname !== prec_serv) {
+                    // Demander la date
+                    sendMessage(hostname);
+                    // Sauvegarder le serveur
+                    prec_serv = hostname;
+                }
+            } else {
+                // Sinon envoyer le message
+                sendMessage(hostname);
+                // Et sauvegarder le serveur
+                prec_serv = hostname;
+            }
         });
     }
 });
