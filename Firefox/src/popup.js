d45659bea07779a9644db6ab901913262e52015f
Jules Lassara (laptop) <jules.lssr@gmail.com>
2018-01-10 12:06:01 +0100

    Pas grand chose, bcp de frontend

--- Firefox/src/popup.js
+++ Firefox/src/popup.js
@@ -50,3 +50,22 @@ ui.btn_unfollow.addEventListener('click', () => {
       })
 })
 */
+
+//TODO debug
+// Quand on clique sur "Développer" pour afficher la liste des sites
+ui.btn_chevron_down.addEventListener('click', () => {
+  url_parser.getCurrentHostname()
+      .then(hostname => {
+        ui.btn_chevron_down.hidden = true;
+        ui.btn_chevron_up.hidden = false;
+        ui.sites_list.hidden = false;
+      })
+})
+
+//TODO debug
+// Quand on clique sur "Réduire" pour cacher la liste des sites
+ui.btn_chevron_up.addEventListener('click', () => {
+  ui.btn_chevron_down.hidden = false;
+  ui.btn_chevron_up.hidden = true;
+  ui.sites_list.hidden = true;
+})
--- Firefox/src/ui.js
+++ Firefox/src/ui.js
@@ -1,13 +1,21 @@
 const storage = require('./storage')
 const url_parser = require('./url')
 
+// Boutons
 let btn_follow = document.getElementById('follow')
 let btn_unfollow = document.getElementById('unfollow')
-//let btn_unfollowall = document.getElementById('unfollowall')
+var btn_chevron_down = document.getElementById('chevron-down')
+var btn_chevron_up = document.getElementById('chevron-up')
+//TODO: var btn_unfollowall = document.getElementById('unfollowall')
+
+// Autres éléments
 var followed = document.getElementById('followed')
 var no_stapling = document.getElementById('no_stapling')
 var unfollowed = document.getElementById('unfollowed')
 var sites_list = document.getElementById('sites')
+//Oui c'est pas beau mais je fais comment moi ?
+// TODO ajouter id pour éditer/supprimer le site en question
+var edit_site = "<i class=\"fa fa-pencil edit\"></i> <i class=\"fa fa-remove edit\"></i>"
 
 /**
  * Affiche la liste des sites dans un tableau
@@ -16,7 +24,8 @@ function printSites() {
     if (storage.isAvailable()) {
         // Vide l'ancien tableau
         clear()
-        let row = sites_list.insertRow(-1)
+
+        let row = sites_list.insertRow(0);
 
         if (storage.isEmpty()) {
             row.insertCell(0).innerHTML += "Aucun site suivi.";
@@ -26,9 +35,17 @@ function printSites() {
                 let critical_age = storage.getCriticalAge(hostname)
                 row.insertCell(0).innerHTML += hostname
                 row.insertCell(1).innerHTML += critical_age
-                row = sites_list.insertRow(-1)
+                row.insertCell(2).innerHTML += edit_site
+                row = sites_list.insertRow(0);
             }
         }
+
+        //TODO: Entête du tableau direct dans le html
+        row = sites_list.insertRow(0);
+        row.insertCell(0).innerHTML += "<b>Site</b>"
+        row.insertCell(1).innerHTML += "<b>Ancienneté critique</b>"
+        row.insertCell(2).innerHTML += ""
+
     } else {
         storageUnavailableError()
     }
@@ -101,6 +118,9 @@ module.exports = {
     printSites,
     btn_follow,
     btn_unfollow,
+    btn_chevron_down,
+    btn_chevron_up,
+    sites_list,
     showFollowed,
     showUnfollowed,
     showDisabled,
