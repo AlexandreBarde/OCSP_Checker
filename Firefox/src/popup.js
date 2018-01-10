a1ab2b51eefa9c7946be732ccce0e4dcac653886
Mathieu Mandret <mathieu.mandret119@gmail.com>
2018-01-10 16:59:58 +0100

    Fix vérif date

--- Firefox/src/popup.js
+++ Firefox/src/popup.js
@@ -11,21 +11,19 @@ const stor = require('./storage')
 // récupérer le nom d'hote
 url_parser.getCurrentHostname()
     .then(hostname => {
-        console.log(hostname)
         browser.runtime.sendMessage(hostname)
             .then(rep => {
-                console.log(rep)
                 ui.printSites()
                 // Si le site n'utilise pas OCSP Stapling
-                if (!date.isDate(rep.text)) {
+                if (!date.isDate(rep)) {
                     // Ne pas afficher la div permettant de le suivre
                     ui.showDisabled()
                 } else {
                     // Sinon si le site n'est pas déjà suivi, lui proposer de le suivre
-                    if(stor.getSite(hostname) == null) {
-                      ui.showUnfollowed()
+                    if (stor.getSite(hostname) == null) {
+                        ui.showUnfollowed()
                     } else {
-                      ui.showFollowed()
+                        ui.showFollowed()
                     }
                 }
             })
@@ -40,23 +38,23 @@ ui.btn_follow.addEventListener('click', () => {
             var mins = document.getElementById("minutes").value;
             var secs = document.getElementById("seconds").value;
 
-            if(!isNaN(days) && !isNaN(hours) && !isNaN(mins) && !isNaN(secs)
-               && (days >= 0 && hours >= 0 && mins >= 0 && secs >= 0)
-               && (days + hours + mins + secs > 0)) {
+            if (!isNaN(days) && !isNaN(hours) && !isNaN(mins) && !isNaN(secs)
+                && (days >= 0 && hours >= 0 && mins >= 0 && secs >= 0)
+                && (days + hours + mins + secs > 0)) {
 
-              var time = moment.duration({
-                  seconds: secs,
-                  minutes: mins,
-                  hours: hours,
-                  days: days,
-                  weeks: 0,
-                  months: 0,
-                  years: 0
+                var time = moment.duration({
+                    seconds: secs,
+                    minutes: mins,
+                    hours: hours,
+                    days: days,
+                    weeks: 0,
+                    months: 0,
+                    years: 0
                 });
 
-              ui.follow(hostname, time.asSeconds());
+                ui.follow(hostname, time.asSeconds());
             } else {
-              //TODO: message d'erreur
+                //TODO: message d'erreur
             }
         })
 })
@@ -71,24 +69,24 @@ ui.btn_unfollow.addEventListener('click', () => {
 
 // Quand on clique sur "Vider la liste des sites"
 ui.btn_unfollowall.addEventListener('click', () => {
-  ui.unfollowAllSites()
+    ui.unfollowAllSites()
 })
 
 // Quand on clique sur "Développer" pour afficher la liste des sites
 ui.btn_chevron_down.addEventListener('click', () => {
-  url_parser.getCurrentHostname()
-      .then(hostname => {
-        ui.btn_chevron_down.hidden = true;
-        ui.btn_chevron_up.hidden = false;
-        ui.sites_list.hidden = false;
-        ui.btn_unfollowall.hidden = false;
-      })
+    url_parser.getCurrentHostname()
+        .then(hostname => {
+            ui.btn_chevron_down.hidden = true;
+            ui.btn_chevron_up.hidden = false;
+            ui.sites_list.hidden = false;
+            ui.btn_unfollowall.hidden = false;
+        })
 })
 
 // Quand on clique sur "Réduire" pour cacher la liste des sites
 ui.btn_chevron_up.addEventListener('click', () => {
-  ui.btn_chevron_down.hidden = false;
-  ui.btn_chevron_up.hidden = true;
-  ui.sites_list.hidden = true;
-  ui.btn_unfollowall.hidden = true;
+    ui.btn_chevron_down.hidden = false;
+    ui.btn_chevron_up.hidden = true;
+    ui.sites_list.hidden = true;
+    ui.btn_unfollowall.hidden = true;
 })
