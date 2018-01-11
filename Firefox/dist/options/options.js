4efb1097aa003801ce20ac2af28aa4f1a052ee4b
Alexandre <barde.alexandre@yahoo.fr>
2018-01-11 23:24:08 +0100

    menage

--- Firefox/dist/options/options.js
+++ Firefox/dist/options/options.js
@@ -2,9 +2,11 @@ function save_options()
 {
   // Récupération des différents champs
   var couleur = document.getElementById('couleur').value;
+  var couleurPartieTexte = document.getElementById('couleurPartieTexte').value;
   var couleurTexte = document.getElementById('couleurTexte').value;
   var emplacement = document.getElementById('emplacement').value;
   var opacite = document.getElementById('opacite').value;
+  var temps = document.getElementById('temps').value;
   var affichageNotif = document.getElementById('affNotif').checked;
   // Stockage dans chrome storage
   chrome.storage.sync.set({
@@ -12,6 +14,8 @@ function save_options()
     emplacementNotif: emplacement,
     affichageNotifPerm: affichageNotif,
     couleurTexteNotif: couleurTexte,
+    couleurPartieTexteNotif: couleurPartieTexte,
+    tempsNotif: temps,
     opaciteNotif: opacite
   }, function() {
     // Récupération du div status
@@ -36,15 +40,19 @@ function restore_options()
   chrome.storage.sync.get({
     couleurNotif: '#fffff',
     couleurTexteNotif: '#fffff',
+    couleurPartieTexteNotif: 'black',
     emplacementNotif: 'haut_gauche',
     opaciteNotif: "95",
+    temps: 2,
     affichageNotifPerm: true
   }, function(items) {
     // Attribution des valeurs aux différents champs de la page
     document.getElementById('emplacement').value = items.emplacementNotif;
     document.getElementById('couleur').value = items.couleurNotif;
     document.getElementById('opacite').value = items.opaciteNotif;
+    document.getElementById('couleurPartieTexte').value = items.couleurPartieTexteNotif;
     document.getElementById('couleurTexte').value = items.couleurTexteNotif;
+    document.getElementById('temps').value = items.tempsNotif;
     document.getElementById('affNotif').checked = items.affichageNotifPerm;
   });
 }
