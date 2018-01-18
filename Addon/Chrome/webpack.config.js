9f3a25afd665bb9074301c778349e71c92adb6e7
Mathieu <mathieu.mandret119@gmail.com>
2018-01-18 01:08:24 +0000

    Tests unitaires et elimination de momentjs dans le module date

--- Addon/Chrome/webpack.config.js
+++ Addon/Chrome/webpack.config.js
@@ -4,17 +4,17 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
     // Fichiers en entrée
     entry: {
-        background: path.join(__dirname, "src", "background.js"),
-        content: path.join(__dirname, "src", "content.js"),
-        popup: path.join(__dirname, "src", "popup.js")
+        background: path.join(__dirname, 'src', 'background.js'),
+        content: path.join(__dirname, 'src', 'content.js'),
+        popup: path.join(__dirname, 'src', 'popup.js')
     },
     // Fichier compilés en sortie
     output: {
-        path: path.join(__dirname, "dist", "js"),
-        filename: "[name].bundle.js"
+        path: path.join(__dirname, 'dist', 'js'),
+        filename: '[name].bundle.js'
     },
 
-//    plugins: [
-//        new UglifyJsPlugin()
-//    ]
+    plugins: [
+        new UglifyJsPlugin()
+    ]
 }
